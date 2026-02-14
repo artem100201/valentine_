c9c806f0748f49c119d0f6d39a23d42b169d8742
artem100201 <artem100201@yandex.ru>
2026-02-14 12:25:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "Ураааа!";
 
     catImg.src = "cat_dance.gif";
 
